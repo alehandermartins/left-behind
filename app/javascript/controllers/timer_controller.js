762f07a7b968c64508268955462c1ea8b3acfbde
alehandermartins <alehander.marti@gmail.com>
2023-05-04 07:49:29 +0200

    Working clock

--- app/javascript/controllers/timer_controller.js
+++ app/javascript/controllers/timer_controller.js
@@ -4,17 +4,27 @@ export default class extends Controller {
   static values = { date: Number }
 
   connect() {
+
+    if(this.dateValue == 0)
+      return
+
+    this.updateClock(this.dateValue)
+    setInterval(this.updateClock, 1000, this.dateValue)
+  }
+
+  updateClock(start_time) {
     const now = new Date()
-    const seconds_since_start = Math.round(now.getTime() / 1000) - this.dateValue
+    const seconds_since_start = Math.round(now.getTime() / 1000) - start_time
     var minutes = Math.floor(seconds_since_start / 60)
     var seconds = seconds_since_start % 60
 
-    if(minutes < 10)
+    if (minutes < 10)
       minutes = "0" + minutes
 
-    if(seconds < 10)
+    if (seconds < 10)
       seconds = "0" + seconds
 
-    this.element.textContent = minutes + ":" + seconds
+    var elem = document.getElementById("clock")
+    elem.innerHTML = minutes + ":" + seconds
   }
 }
